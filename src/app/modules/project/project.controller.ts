e1621a75708794f02e3298b93c14d5123cc5e70d
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-09-28 13:14:08 +0600

    get-all-project-controller

--- src/app/modules/project/project.controller.ts
+++ src/app/modules/project/project.controller.ts
@@ -27,4 +27,24 @@ const getProjectById = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const ProjectController = { createProject, getProjectById };
+// get all project
+const getAllProjects = catchAsync(async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const { projects, meta } = await ProjectService.getAllProjects(page, limit);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Projects fetched successfully",
+    data: projects,
+    meta,
+  });
+});
+
+export const ProjectController = {
+  createProject,
+  getProjectById,
+  getAllProjects,
+};
