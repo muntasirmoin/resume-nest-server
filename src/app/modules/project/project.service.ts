b882c3bf5bc5bb8ce3f81707c104da27137be93b
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-09-28 13:07:26 +0600

    get-project-service

--- src/app/modules/project/project.service.ts
+++ src/app/modules/project/project.service.ts
@@ -4,6 +4,7 @@ import AppError from "../../helpers/AppError";
 
 const prisma = new PrismaClient();
 
+// create project
 const createProject = async (payload: IProject) => {
   const authorExists = await prisma.user.findUnique({
     where: { id: payload.authorId },
@@ -27,6 +28,32 @@ const createProject = async (payload: IProject) => {
       updatedAt: true,
     },
   });
+
+  return project;
+};
+
+// get single project
+
+const getProjectById = async (projectId: string) => {
+  const project = await prisma.project.findUnique({
+    where: { id: projectId },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      thumbnail: true,
+      projectLink: true,
+      liveSite: true,
+      features: true,
+      authorId: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  if (!project) {
+    throw new AppError(404, "Project not found");
+  }
 
   return project;
 };
